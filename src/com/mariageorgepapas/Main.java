ae6136e4973773d4b4162b031a316c5016e02169
morpheus <mariageorgepapas@gmail.com>
2020-04-25 00:02:02 -0500

    Bonus Part of challenge

--- src/com/mariageorgepapas/Main.java
+++ src/com/mariageorgepapas/Main.java
@@ -37,7 +37,22 @@ public class Main {
         if(minutes>=0 && (seconds>=0 && seconds<=59)) {
             int hours = minutes / 60;
             int minutesLeft = minutes % 60;
-            return hours + "h " + minutesLeft + "m " + seconds + "s";
+
+            String hoursString = hours + "h";
+            if(hours < 10) {
+                hoursString = "0" + hoursString;
+            }
+
+            String minutesString = minutesLeft + "m";
+            if(minutesLeft < 10) {
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if(seconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+            return hoursString + " " + minutesString + " " + secondsString;
         }
         return "Invalid value";
     }
